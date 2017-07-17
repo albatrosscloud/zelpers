afe6b0faffdeaeacd7585011e2b5616366816175
jose-ea <jose@albatross.pe>
2017-07-17 12:48:54 -0500

    fix

---
+++ src/main/java/pe/albatross/zelpers/security/oauth/AuthProvidersEnum.java
@@ -0,0 +1,7 @@
+package pe.albatross.zelpers.security.oauth;
+
+public enum AuthProvidersEnum {
+
+    HTTP, GOOGLE , FACEBOOK, TWITTER, LINKEDIN, REDDIT, GITHUB, BITBUCKET, ALBATROSS;
+
+}
--- src/main/java/pe/albatross/zelpers/security/oauth/OAuthProvidersEnum.java
+++
@@ -1,7 +0,0 @@
-package pe.albatross.zelpers.security.oauth;
-
-public enum OAuthProvidersEnum {
-
-    GOOGLE , FACEBOOK, TWITTER, LINKEDIN, REDDIT, GITHUB, BITBUCKET, ALBATROSS;
-
-}
