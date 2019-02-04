80734f4143d11a6746fd98f5849b8da127b468ae
diesgut <diego@albatross.pe>
2019-02-04 14:06:10 -0500

    esquema opta

--- src/main/java/pe/albatross/zelpers/miscelanea/TypesUtil.java
+++ src/main/java/pe/albatross/zelpers/miscelanea/TypesUtil.java
@@ -397,4 +397,19 @@ public class TypesUtil {
         }
     }
 
+    public String capitalizeWord(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        str = str.trim().replaceAll("\\s{2,}", " ").trim();
+        String words[] = str.split("\\s");
+        String capitalizeWord = "";
+        for (String w : words) {
+            String first = w.substring(0, 1);
+            String afterfirst = w.substring(1);
+            capitalizeWord += first.toUpperCase() + afterfirst.toLowerCase() + " ";
+        }
+        return capitalizeWord.trim();
+    }
+
 }
